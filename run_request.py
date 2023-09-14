6dbc19fb4da3ee3dcdec5947e0d356eafcd5fc5c
Emilio Becerra <emiliobecerra@128.219.21.198.tigernet.wifi.dyn.clemson.edu>
2023-09-14 11:45:18 -0400

    loop through many ids

---
+++ run_request.py
@@ -0,0 +1,42 @@
+
+import requests
+import json
+import os
+import pandas
+import time
+
+if not os.path.exists("json_files"):
+  os.mkdir("json_files")
+
+
+access_point = "https://api.github.com"
+
+f = open("token", "r")
+token = f.read()
+f.close()
+
+#read a .csv file, and put it in a dataset
+id_list = pandas.read_csv("seed.csv")
+#Get a spreadsheet of one column
+id_list = id_list['ghid']
+
+
+github_session = requests.Session()
+github_session.auth = ("emiliobecerra", token)
+
+#All of this is in a for loop:
+response_text = github_session.get(access_point + "/rate_limit").text
+print(json.loads(response_text))
+
+for user_id in id_list: 
+	# user_id = "erinata"
+	print(user_id)
+	response_text = github_session.get(access_point + "/users/" + user_id).text
+
+	json_text = json.loads(response_text)
+
+	file_name = "json_files/" + user_id + ".json"
+	f = open(file_name, "w")
+	f.write(json.dumps(json_text))
+	f.close()
+	time.sleep(1)
