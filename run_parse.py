d2842cddc8513d72ad8ed6b907ce34c2b960c16d
Emilio Becerra <emiliobecerra@97.221.21.198.tigernet.wifi.dyn.clemson.edu>
2023-09-19 12:13:00 -0400

    four column csv

--- run_parse.py
+++ run_parse.py
@@ -1,6 +1,7 @@
 import json #read csv files
 import pandas #for data structures
 import os 
+import glob
 
 if not os.path.exists("parsed_files"):
   os.mkdir("parsed_files")
@@ -10,42 +11,42 @@ if not os.path.exists("parsed_files"):
 
 dataset = pandas.DataFrame()
 
-#One person parse
-json_file_name = "json_files/dAAAb.json"
+#create a csv with columns
 
-f = open(json_file_name, "r")
-json_data = json.load(f)
-f.close()
+for json_file_name in glob.glob("json_files/*.json"):
+	# json_file_name = "json_files/dAAAb.json"
 
-# print(json_data)
+	f = open(json_file_name, "r")
+	json_data = json.load(f)
+	f.close()
 
-gh_id = json_data['login']
-gh_number_id = json_data['id']
-updated_at = json_data['updated_at']
-followers = json_data['followers']
+	# print(json_data)
+	# homework: 
+	gh_id = json_data['login']
+	gh_number_id = json_data['id']
+	updated_at = json_data['updated_at']
+	followers = json_data['followers']
 
-print(gh_id)
-print(gh_number_id)
-print(updated_at)
-print(followers)
+	print(gh_id)
+	print(gh_number_id)
+	print(updated_at)
+	print(followers)
 
-#
-row = pandas.DataFrame.from_records(
-	[
-	{
-		'gh_id' : gh_id,
-		'gh_number_id' : gh_number_id,
-		'updated_at' : updated_at,
-		'followers' : followers
-	}
-	]
-	)
+	#
+	row = pandas.DataFrame.from_records(
+		[
+		{
+			'gh_id' : gh_id,
+			'gh_number_id' : gh_number_id,
+			'updated_at' : updated_at,
+			'followers' : followers
+		}])
 
-print(row)
-#merge dataset with row
-dataset = pandas.concat([dataset, row])
+	print(row)
+		#merge dataset with row, concat matches columns
+	dataset = pandas.concat([dataset, row])
 
 dataset.to_csv("parsed_files/github_user_data.csv"
-						, index=False)
+							, index=False)
 
 
